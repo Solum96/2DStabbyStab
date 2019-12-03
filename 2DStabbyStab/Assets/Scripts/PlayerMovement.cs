a0d1bdad66b89d8be29d32721e239f8ec4db0df0
Nils Lundell <nils.solumfaeste@gmail.com>
2019-12-03 09:32:30 +0100

    Switched Target Platform to PC

---
+++ 2DStabbyStab/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody myRigidbody;
+    public float speed;
+    // Update is called once per frame
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown("w"))
+        {
+            myRigidbody.velocity = transform.up * speed;
+        }
+    }
+}
