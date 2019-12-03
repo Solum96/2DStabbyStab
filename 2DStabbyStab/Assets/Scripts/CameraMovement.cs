6caafa4721591e467a29bcc0e9a101a4a13e50be
Nils Lundell <nils.solumfaeste@gmail.com>
2019-12-03 11:37:17 +0100

    Added Jumping and camerafollow

---
+++ 2DStabbyStab/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float yAxis;
+    public float zAxis;
+    public float smoothing;
+    // Update is called once per frame
+    void Start()
+    {
+        yAxis = transform.position.y;
+        zAxis = transform.position.z;
+    }
+    void LateUpdate()
+    {
+        if(transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y + yAxis, target.position.z + zAxis);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
