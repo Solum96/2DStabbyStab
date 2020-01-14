3a8c29465e639dce6faef75312c2b0cad20b1a88
Nils Lundell <nils.solumfaeste@gmail.com>
2020-01-14 12:45:44 +0100

    Added EnemyCombat Script

---
+++ 2DStabbyStab/Assets/Scripts/EnemyCombat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCombat : MonoBehaviour
+{
+    Animator animator;
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        animator.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        animator.SetBool("isDead", true);
+        
+        GetComponent<CapsuleCollider>().enabled = false;
+        GameObject.Destroy(gameObject, 2.5f);
+    }
+}
